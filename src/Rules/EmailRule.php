b86bc072a38fc09d42e5a5c9e831fd2b63648a8a
Nick Mispoulier <nick@proclame.be>
2024-01-21 21:39:11 +0100

    Use egulias email validator for email validation

--- src/Rules/EmailRule.php
+++ src/Rules/EmailRule.php
@@ -2,17 +2,21 @@
 
 namespace Proclame\Validator\Rules;
 
+use Egulias\EmailValidator\EmailValidator;
+use Egulias\EmailValidator\Validation\RFCValidation;
+
 class EmailRule extends AbstractRule
 {
     protected string $errorMessage = 'Not a valid e-mail address';
+    private EmailValidator $validator;
+
+    public function __construct()
+    {
+        $this->validator = new EmailValidator();
+    }
 
     protected function rule($value): bool
     {
-        return filter_var($value, FILTER_VALIDATE_EMAIL) !== false;
-    }
-
-    protected function sanitized($value)
-    {
-        return filter_var($value, FILTER_SANITIZE_EMAIL);
+        return $this->validator->isValid($value, new RFCValidation());
     }
 }
